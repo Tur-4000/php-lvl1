c80850e33e75085168a0e798250524588c0c9e50
Turbanov Valeriy <tur.4000@gmail.com>
2019-07-02 13:25:36 +0300

    phpcs

--- src/cli.php
+++ src/cli.php
@@ -8,5 +8,5 @@ function run()
 {
     line("Welcome to the Brain Games!");
     $playerName = \cli\prompt('May I have your name?');
-    line("Hello, %s!", $playerName); 
+    line("Hello, %s!", $playerName);
 }
