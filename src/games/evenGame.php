8265c4b9c9d4aadae92de4513d6061bf23461751
Turbanov Valeriy <tur.4000@gmail.com>
2019-07-03 20:15:49 +0300

    refactor

--- src/games/evenGame.php
+++ src/games/evenGame.php
@@ -5,6 +5,7 @@ namespace BrainGames\Games;
 use function \cli\line;
 use function \cli\prompt;
 use function BrainGames\Cli\greeting;
+use function BrainGames\Games\game;
 
 function runEvenGame()
 {
@@ -12,42 +13,14 @@ function runEvenGame()
 
     $playerName = greeting();
 
-    $result = evenGame();
-
-    if ($result) {
-        line("Congratulations, ${playerName}!");
-    } else {
-        line("Let's try again, ${playerName}!");
-    }
-}
-
-function evenGame()
-{
-    $i = 0;
-
-    while ($i < 3) {
+    $question = function () {
         $num = rand(0, 99);
-        line("Question: ${num}");
-        $answer = prompt('Your answer: ');
+        $question = "${num}";
 
-        if ($num % 2 === 0) {
-            if ($answer === 'yes') {
-                line("Correct!");
-            } else {
-                line("'${answer}' is wrong answer ;(. Correct answer was 'yes'.");
-                return false;
-            }
-        } else {
-            if ($answer === 'no') {
-                line("Correct!");
-            } else {
-                line("'${answer}' is wrong answer ;(. Correct answer was 'no'.");
-                return false;
-            }
-        }
+        line("Question: ${question}");
 
-        $i += 1;
-    }
+        return ($num % 2 === 0) ? 'yes' : 'no';
+    };
 
-    return true;
+    game($playerName, $question);
 }
